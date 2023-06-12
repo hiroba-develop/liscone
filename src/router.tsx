cdfd2cccb928de7c11210b946a04caefae959682
smchoi98 <smchoi@in-soft.co.kr>
2023-06-12 11:45:02 +0900

    make menu tree

--- src/router.tsx
+++ src/router.tsx
@@ -14,69 +14,18 @@ const Loader = (Component) => (props) =>
     </Suspense>
   );
 
-// Pages
-
-// Applications
-
-const Messenger = Loader(
-  lazy(() => import("src/content/applications/Messenger"))
-);
-const Transactions = Loader(
-  lazy(() => import("src/content/applications/Transactions"))
-);
-const UserProfile = Loader(
-  lazy(() => import("src/content/applications/Users/profile"))
-);
-const UserSettings = Loader(
-  lazy(() => import("src/content/applications/Users/settings"))
-);
-
-// Components
-
-const Buttons = Loader(
-  lazy(() => import("src/content/pages/Components/Buttons"))
-);
-const Modals = Loader(
-  lazy(() => import("src/content/pages/Components/Modals"))
-);
-const Accordions = Loader(
-  lazy(() => import("src/content/pages/Components/Accordions"))
-);
-const Tabs = Loader(lazy(() => import("src/content/pages/Components/Tabs")));
-const Badges = Loader(
-  lazy(() => import("src/content/pages/Components/Badges"))
-);
-const Tooltips = Loader(
-  lazy(() => import("src/content/pages/Components/Tooltips"))
-);
-const Avatars = Loader(
-  lazy(() => import("src/content/pages/Components/Avatars"))
-);
-const Cards = Loader(lazy(() => import("src/content/pages/Components/Cards")));
-const Forms = Loader(lazy(() => import("src/content/pages/Components/Forms")));
-
-// Status
-
-const Status404 = Loader(
-  lazy(() => import("src/content/pages/Status/Status404"))
-);
-const Status500 = Loader(
-  lazy(() => import("src/content/pages/Status/Status500"))
-);
-const StatusComingSoon = Loader(
-  lazy(() => import("src/content/pages/Status/ComingSoon"))
-);
-const StatusMaintenance = Loader(
-  lazy(() => import("src/content/pages/Status/Maintenance"))
-);
 
 // Sign
-
 const SignIn = Loader(lazy(() => import("src/content/signIn")));
 
-// TaskBoard
-
-const TaskBoard = Loader(lazy(() => import("src/content/taskBoard")));
+// Applications
+const DashBoard = Loader( lazy(() => import("src/content/applications/DashBoard")) );
+const ActionLog = Loader( lazy(() => import("src/content/applications/ActionLog")) );
+const CompanyList = Loader( lazy(() => import("src/content/applications/CompanyList")) );
+const ContactPersonList = Loader( lazy(() => import("src/content/applications/ContactPersonList")) );
+const Lists = Loader( lazy(() => import("src/content/applications/Lists")) );
+const Reports = Loader( lazy(() => import("src/content/applications/Reports")) );
+const Setting = Loader( lazy(() => import("src/content/applications/Setting")) );
 
 const routes: RouteObject[] = [
   {
@@ -86,151 +35,106 @@ const routes: RouteObject[] = [
       {
         path: "/",
         element: <SignIn />,
-      },
-      {
-        path: "overview",
-        element: <Navigate to="/" replace />,
-      },
-      {
-        path: "status",
-        children: [
-          {
-            path: "",
-            element: <Navigate to="404" replace />,
-          },
-          {
-            path: "404",
-            element: <Status404 />,
-          },
-          {
-            path: "500",
-            element: <Status500 />,
-          },
-          {
-            path: "maintenance",
-            element: <StatusMaintenance />,
-          },
-          {
-            path: "coming-soon",
-            element: <StatusComingSoon />,
-          },
-        ],
-      },
-      {
-        path: "*",
-        element: <Status404 />,
-      },
+      }
     ],
   },
   {
-    path: "taskBoard",
+    path: "dashBoard",
     element: <SidebarLayout />,
     children: [
       {
         path: "",
-        element: <Navigate to="taskBoard" replace />,
+        element: <Navigate to="dashBoard" replace />,
       },
       {
-        path: "taskBoard",
-        element: <TaskBoard />,
-      },
-      {
-        path: "messenger",
-        element: <Messenger />,
+        path: "dashBoard",
+        element: <DashBoard />,
       },
     ],
   },
   {
-    path: "account",
-    element: <NonSidebarLayout />,
-    children: [
-      {
-        path: "signIn",
-        element: <SignIn />,
-      },
-      {
-        path: "signUp",
-        element: <SignIn />,
-      },
-    ],
-  },
-  {
-    path: "management",
+    path: "companyList",
     element: <SidebarLayout />,
     children: [
       {
         path: "",
-        element: <Navigate to="transactions" replace />,
+        element: <Navigate to="companyList" replace />,
       },
       {
-        path: "transactions",
-        element: <Transactions />,
-      },
-      {
-        path: "profile",
-        children: [
-          {
-            path: "",
-            element: <Navigate to="details" replace />,
-          },
-          {
-            path: "details",
-            element: <UserProfile />,
-          },
-          {
-            path: "settings",
-            element: <UserSettings />,
-          },
-        ],
+        path: "companyList",
+        element: <CompanyList />,
       },
     ],
   },
   {
-    path: "/components",
+    path: "contactPersonList",
     element: <SidebarLayout />,
     children: [
       {
         path: "",
-        element: <Navigate to="buttons" replace />,
+        element: <Navigate to="contactPersonList" replace />,
       },
       {
-        path: "buttons",
-        element: <Buttons />,
+        path: "contactPersonList",
+        element: <ContactPersonList />,
+      },
+    ],
+  },
+  {
+    path: "lists",
+    element: <SidebarLayout />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to="lists" replace />,
       },
       {
-        path: "modals",
-        element: <Modals />,
+        path: "lists",
+        element: <Lists />,
+      },
+    ],
+  },
+  {
+    path: "reports",
+    element: <SidebarLayout />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to="reports" replace />,
       },
       {
-        path: "accordions",
-        element: <Accordions />,
+        path: "reports",
+        element: <Reports />,
+      },
+    ],
+  },
+  {
+    path: "actionLog",
+    element: <SidebarLayout />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to="actionLog" replace />,
       },
       {
-        path: "tabs",
-        element: <Tabs />,
+        path: "actionLog",
+        element: <ActionLog />,
+      },
+    ],
+  },
+  {
+    path: "setting",
+    element: <SidebarLayout />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to="setting" replace />,
       },
       {
-        path: "badges",
-        element: <Badges />,
-      },
-      {
-        path: "tooltips",
-        element: <Tooltips />,
-      },
-      {
-        path: "avatars",
-        element: <Avatars />,
-      },
-      {
-        path: "cards",
-        element: <Cards />,
-      },
-      {
-        path: "forms",
-        element: <Forms />,
+        path: "setting",
+        element: <Setting />,
       },
     ],
   },
 ];
-
 export default routes;
