e66be12e78bbdcd4d13b1fcc147d2cd75c74ebc9
JushinSeo <jushin@cydas.com>
2023-08-02 18:04:12 +0900

    リスト名重複防止

--- src/content/applications/PopUp/ListCreate/index.tsx
+++ src/content/applications/PopUp/ListCreate/index.tsx
@@ -1,10 +1,13 @@
 import CloseIcon from "@mui/icons-material/Close";
 import {
+  Alert,
+  AlertTitle,
   Box,
   Button,
   IconButton,
   MenuItem,
   Modal,
+  Snackbar,
   TextField,
   Typography,
 } from "@mui/material";
@@ -19,6 +22,7 @@ import {
 } from "src/utility/http/ApiService";
 import { membersAtom } from "src/utility/recoil/comp/Members.atom";
 import { productsAtom } from "src/utility/recoil/comp/Products.atom";
+import ErrorIcon from "../../icon/ErrorIcon";
 
 const ListCreate = ({
   listCreateOpen,
@@ -44,7 +48,8 @@ const ListCreate = ({
   const handleProductSelect = (e) => {
     setProductSelected(e.target.value);
   };
-
+  const [isSnackBarError, setIsTextError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const { mutate, isError } = useWrapMuation<any, any>(
     ["createlist"],
     async (data) => {
@@ -65,7 +70,8 @@ const ListCreate = ({
       },
       onError: (error) => {
         commonErrorCallback(error);
-        alert(error.response.data.message);
+        setErrorMessage(error.response.data.message);
+        setIsTextError(true);
       },
     }
   );
@@ -104,142 +110,187 @@ const ListCreate = ({
     };
 
     return (
-      <Modal open={listCreateOpen} onClose={editListCreateClose}>
-        <Box sx={editModal}>
-          <Typography sx={editModalTitle}>リストを作成</Typography>
-          <Box
-            sx={{
-              position: "absolute",
-              top: "0",
-              right: "0",
-              textAlign: "right",
-              color: "white",
-            }}
-          >
-            <IconButton onClick={editListCreateClose}>
-              <CloseIcon sx={{ color: "white" }} />
-            </IconButton>
+      <>
+        <Modal open={listCreateOpen} onClose={editListCreateClose}>
+          <Box sx={editModal}>
+            {isError && (
+              <Snackbar
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "center",
+                }}
+                open={isSnackBarError}
+                autoHideDuration={300}
+                onClose={() => setIsTextError(false)}
+              >
+                <Alert
+                  severity="error"
+                  sx={{
+                    display: "flex",
+                    background: "#231F20",
+                    borderRadius: 4,
+                    padding: "16px 24px",
+                  }}
+                  iconMapping={{
+                    error: <ErrorIcon fontSize="inherit" />,
+                  }}
+                >
+                  <AlertTitle>
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        color: "#FFFFFF",
+                      }}
+                    >
+                      Error
+                    </Typography>
+                  </AlertTitle>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      color: "#FFFFFF",
+                    }}
+                  >
+                    {errorMessage}
+                  </Typography>
+                </Alert>
+              </Snackbar>
+            )}
+            <Typography sx={editModalTitle}>リストを作成</Typography>
+            <Box
+              sx={{
+                position: "absolute",
+                top: "0",
+                right: "0",
+                textAlign: "right",
+                color: "white",
+              }}
+            >
+              <IconButton onClick={editListCreateClose}>
+                <CloseIcon sx={{ color: "white" }} />
+              </IconButton>
+            </Box>
+            <Box component={"form"} onSubmit={(e) => createSalesList(e)}>
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "20%",
+                  left: "3%",
+                  fontSize: "20px",
+                  pt: 1,
+                }}
+              >
+                リスト名：
+              </Box>
+              <TextField
+                value={salesListNameValue}
+                onChange={saveSalesListName}
+                variant="outlined"
+                size="small"
+                inputProps={{
+                  style: {
+                    width: 400,
+                  },
+                }}
+                sx={{
+                  position: "absolute",
+                  top: "20%",
+                  left: "25%",
+                  ml: 1,
+                  mt: 1,
+                }}
+              />
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "40%",
+                  left: "3%",
+                  fontSize: "20px",
+                  pt: 1,
+                }}
+              >
+                ユーザー：
+              </Box>
+              <TextField
+                sx={{
+                  position: "absolute",
+                  top: "40%",
+                  left: "25%",
+                  minWidth: "200px",
+                  ml: 1,
+                  mt: 1,
+                }}
+                id="members"
+                select
+                value={MemberSelected}
+                style={{ width: 200, marginTop: 10 }}
+                onChange={handleMemberSelect}
+              >
+                {members.map((option) => (
+                  <MenuItem value={option.member_id}>
+                    {option.member_name}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "60%",
+                  left: "3%",
+                  fontSize: "20px",
+                  pt: 1,
+                }}
+              >
+                商材：
+              </Box>
+              <TextField
+                sx={{
+                  position: "absolute",
+                  top: "60%",
+                  left: "25%",
+                  minWidth: "200px",
+                  ml: 1,
+                  mt: 1,
+                }}
+                id="products"
+                select
+                value={ProductSelected}
+                style={{ width: 200, marginTop: 10 }}
+                onChange={handleProductSelect}
+              >
+                {products.map((option) => (
+                  <MenuItem value={option.product_number}>
+                    {option.product_name}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <Button
+                disabled={
+                  !(
+                    ProductSelected.length > 0 &&
+                    MemberSelected.length > 0 &&
+                    salesListNameValue.length > 0
+                  )
+                }
+                type="submit"
+                variant="contained"
+                sx={{
+                  borderRadius: 0.5,
+                  backgroundColor: "#109DBC",
+                  position: "absolute",
+                  top: "80%",
+                  left: "25%",
+                  ml: 1,
+                  mt: 1,
+                  minWidth: "300px",
+                }}
+              >
+                リストを作成
+              </Button>
+            </Box>
           </Box>
-          <Box component={"form"} onSubmit={(e) => createSalesList(e)}>
-            <Box
-              sx={{
-                position: "absolute",
-                top: "20%",
-                left: "3%",
-                fontSize: "20px",
-                pt: 1,
-              }}
-            >
-              リスト名：
-            </Box>
-            <TextField
-              value={salesListNameValue}
-              onChange={saveSalesListName}
-              variant="outlined"
-              size="small"
-              inputProps={{
-                style: {
-                  width: 400,
-                },
-              }}
-              sx={{
-                position: "absolute",
-                top: "20%",
-                left: "25%",
-                ml: 1,
-                mt: 1,
-              }}
-            />
-            <Box
-              sx={{
-                position: "absolute",
-                top: "40%",
-                left: "3%",
-                fontSize: "20px",
-                pt: 1,
-              }}
-            >
-              ユーザー：
-            </Box>
-            <TextField
-              sx={{
-                position: "absolute",
-                top: "40%",
-                left: "25%",
-                minWidth: "200px",
-                ml: 1,
-                mt: 1,
-              }}
-              id="members"
-              select
-              value={MemberSelected}
-              style={{ width: 200, marginTop: 10 }}
-              onChange={handleMemberSelect}
-            >
-              {members.map((option) => (
-                <MenuItem value={option.member_id}>
-                  {option.member_name}
-                </MenuItem>
-              ))}
-            </TextField>
-            <Box
-              sx={{
-                position: "absolute",
-                top: "60%",
-                left: "3%",
-                fontSize: "20px",
-                pt: 1,
-              }}
-            >
-              商材：
-            </Box>
-            <TextField
-              sx={{
-                position: "absolute",
-                top: "60%",
-                left: "25%",
-                minWidth: "200px",
-                ml: 1,
-                mt: 1,
-              }}
-              id="products"
-              select
-              value={ProductSelected}
-              style={{ width: 200, marginTop: 10 }}
-              onChange={handleProductSelect}
-            >
-              {products.map((option) => (
-                <MenuItem value={option.product_number}>
-                  {option.product_name}
-                </MenuItem>
-              ))}
-            </TextField>
-            <Button
-              disabled={
-                !(
-                  ProductSelected.length > 0 &&
-                  MemberSelected.length > 0 &&
-                  salesListNameValue.length > 0
-                )
-              }
-              type="submit"
-              variant="contained"
-              sx={{
-                borderRadius: 0.5,
-                backgroundColor: "#109DBC",
-                position: "absolute",
-                top: "80%",
-                left: "25%",
-                ml: 1,
-                mt: 1,
-                minWidth: "300px",
-              }}
-            >
-              リストを作成
-            </Button>
-          </Box>
-        </Box>
-      </Modal>
+        </Modal>
+      </>
     );
   } else {
     return null;
