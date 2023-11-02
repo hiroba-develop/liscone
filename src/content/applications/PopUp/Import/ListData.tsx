fcbf38a14e4a51f29ea0d64076f7e750c367abab
hiroba-develop <134044087+hiroba-develop@users.noreply.github.com>
2023-11-02 21:41:43 +0900

    Revert "不具合修正"
    This reverts commit e155953e9ffe932516ffabb4937eff8d96ba5e32.

--- src/content/applications/PopUp/Import/ListData.tsx
+++ src/content/applications/PopUp/Import/ListData.tsx
@@ -37,86 +37,81 @@ const Import = ({ importPopOpen, setImportPopOpen, sheetDatas }) => {
   });
 
   sheetDataObjects.splice(0, 1);
+  const filteredSheetDataObjects = sheetDataObjects.filter((dataObject) => {
+    return dataObject.corporationName !== undefined;
+  });
 
   const [importData, setImportData] = useState([]);
   useEffect(() => {
-    if (sheetDataObjects.length >= 10000) {
+    if (filteredSheetDataObjects.length >= 10000) {
       alert("インポートできるのは１万件までです。");
       return null;
     }
     const fetchData = async () => {
       // フィルター後のデータを処理
-      for (const dataObject of sheetDataObjects) {
-        if (dataObject.zipCode !== undefined || dataObject.zipCode !== "") {
+      for (const dataObject of filteredSheetDataObjects) {
+        console.log(dataObject);
+        if (dataObject.zipCode !== undefined && dataObject.zipCode !== "") {
           dataObject.zipCode = dataObject.zipCode.substring(0, 3);
         }
-        console.log(dataObject.corporationName);
-        if (
-          dataObject.corporationName === undefined ||
-          dataObject.corporationName === ""
-        ) {
-          alert("会社名は必ず入力してください。");
-          return null;
-        } else {
-          try {
-            const responseImports = await axios.get(
-              `${config().apiUrl}/corporations/searchImport`,
-              {
-                params: {
-                  corporateNumber: dataObject.corporateNumber,
-                  homePage: dataObject.homePage,
-                  corporationName: dataObject.corporationName,
-                  zipCode: dataObject.zipCode,
-                },
-              }
-            );
-
-            if (responseImports.statusText === "OK") {
-              dataObject.corporateDatas = responseImports.data;
-              if (
-                dataObject.capitalStock !== undefined &&
-                dataObject.capitalStock !== ""
-              ) {
-                dataObject.capitalStock = parseInt(dataObject.capitalStock);
-              }
-              if (
-                dataObject.employeeNumber !== undefined &&
-                dataObject.employeeNumber !== ""
-              ) {
-                dataObject.employeeNumber = parseInt(dataObject.employeeNumber);
-              }
-              if (
-                dataObject.establishmentYear !== undefined &&
-                dataObject.establishmentYear !== ""
-              ) {
-                dataObject.establishmentYear = parseInt(
-                  dataObject.establishmentYear
-                );
-              }
-              if (
-                dataObject.salesAmount !== undefined &&
-                dataObject.salesAmount !== ""
-              ) {
-                dataObject.salesAmount = parseInt(dataObject.salesAmount);
-              }
-
-              if (
-                Number.isNaN(dataObject.capitalStock) ||
-                Number.isNaN(dataObject.employeeNumber) ||
-                Number.isNaN(dataObject.establishmentYear) ||
-                Number.isNaN(dataObject.salesAmount)
-              ) {
-                alert("売上、従業員数、設立、資本金は数字のみ入力可能です。");
-                return null;
-              }
+        try {
+          const responseImports = await axios.get(
+            `${config().apiUrl}/corporations/searchImport`,
+            {
+              params: {
+                corporateNumber: dataObject.corporateNumber,
+                homePage: dataObject.homePage,
+                corporationName: dataObject.corporationName,
+                zipCode: dataObject.zipCode,
+              },
             }
-          } catch (error) {
-            commonErrorCallback(error);
+          );
+
+          if (responseImports.statusText === "OK") {
+            dataObject.corporateDatas = responseImports.data;
+            if (
+              dataObject.capitalStock !== undefined &&
+              dataObject.capitalStock !== ""
+            ) {
+              dataObject.capitalStock = parseInt(dataObject.capitalStock);
+            }
+            if (
+              dataObject.employeeNumber !== undefined &&
+              dataObject.employeeNumber !== ""
+            ) {
+              dataObject.employeeNumber = parseInt(dataObject.employeeNumber);
+            }
+            if (
+              dataObject.establishmentYear !== undefined &&
+              dataObject.establishmentYear !== ""
+            ) {
+              dataObject.establishmentYear = parseInt(
+                dataObject.establishmentYear
+              );
+            }
+            if (
+              dataObject.salesAmount !== undefined &&
+              dataObject.salesAmount !== ""
+            ) {
+              dataObject.salesAmount = parseInt(dataObject.salesAmount);
+            }
+
+            if (
+              Number.isNaN(dataObject.capitalStock) ||
+              Number.isNaN(dataObject.employeeNumber) ||
+              Number.isNaN(dataObject.establishmentYear) ||
+              Number.isNaN(dataObject.salesAmount)
+            ) {
+              alert("売上、従業員数、設立、資本金は数字のみ入力可能です。");
+              return null;
+            }
           }
+        } catch (error) {
+          commonErrorCallback(error);
         }
       }
 
-      setImportData(sheetDataObjects);
+      setImportData(filteredSheetDataObjects);
     };
 
     fetchData();
